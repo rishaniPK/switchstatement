23cf41a4da97788606cf495e1658afc5b144d911
Admin <Admin@DESKTOP-89NTJEN>
2019-05-21 11:06:22 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switchstatement
+{
+
+    // This is used to find vowel using switch statements
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any Character : ");
+            char ch = char.Parse(Console.ReadLine());
+            string str =ch.ToString().ToLower();
+            switch(str)
+            {
+               case "a":
+                Console.WriteLine(str +"is a vowel");
+                break;
+                case "e":
+                    Console.WriteLine(str + "is a vowel");
+                    break;
+
+                case "i":
+                    Console.WriteLine(str + "is a vowel");
+                    break;
+
+
+                case "o":
+                    Console.WriteLine(str + "is a vowel");
+                    break;
+
+                case "u":
+                    Console.WriteLine(str + "is a vowel");
+                    break;
+                default:
+                    Console.WriteLine(str + " " + "Is not vowel");
+                    break;
+            }
+        }
+    }
+}
